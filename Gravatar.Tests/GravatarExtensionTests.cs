4385c2a0fdfa23bd3ccd9c003140a9be6c04f683
ricardosribeiro <ricardo.sribeiro@outlook.com>
2021-03-13 20:02:58 -0300

    Implementado suporte a tamanho da imagem

--- Gravatar.Tests/GravatarExtensionTests.cs
+++ Gravatar.Tests/GravatarExtensionTests.cs
@@ -6,18 +6,21 @@ namespace Gravatar.Tests
     public class GravatarExtensionTests
     {
         [TestCategory("Gravatar Tests")]
-        [TestMethod(displayName:"Shoul Validate Gravatar Extension")]
-        [DataRow(null, false)]
-        [DataRow("",false)]
-        [DataRow(" ",false)]
-        [DataRow("a@a",false)]
-        [DataRow("a@ricardo.com",false)]        
-        [DataRow("ricardo.sribeiro@outlook.com",false)]
-        public void TestMethod1(string email, bool status)
+        [TestMethod(displayName: "Should Validate Gravatar Extension")]
+        [DataRow(null, 200, false)]
+        [DataRow("", 200, false)]
+        [DataRow(" ", 200, false)]
+        [DataRow("a@a", 200, false)]
+        [DataRow("a@ricardo.com", 200, false)]
+        [DataRow("ricardo.sribeiro@outlook.com", null, true)]
+        [DataRow("ricardo.sribeiro@outlook.com", 200, true)]
+        public void TestMethod1(string email, int? size, bool status)
         {
-            var result = "https://www.gravatar.com/avatar/64cc168cKcoR2YCKpmVKwTmcYp115bHjtDFYN6";
 
-            Assert.AreEqual((email.ToGravatar()==result),status);
+            var imageSize = size.HasValue ? size.Value.ToString() : "80";
+            var expected = $"https://www.gravatar.com/avatar/64cc168024507d9b541245b42f2f1b36?s={imageSize}";
+            var result = email.ToGravatar(size ?? 80);
+            Assert.AreEqual((result == expected), status);
         }
     }
 }
